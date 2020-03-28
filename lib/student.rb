24933f57b5a4b93dd8dbdfcd02689e4b89b73c7a
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-03-28 01:05:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -69,7 +69,7 @@ class Student
       SELECT * FROM students
       WHERE name = ?
     SQL
-    self.new_from_db(array[1])
+    self.new_from_db(array)
     DB[:conn].execute(sql, name)
   end
 end
